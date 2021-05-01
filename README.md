b955b449b45be31385e4c244baa65f205c85170b
danielquiz1 <danielquiz1@gmail.com>
2021-04-30 22:03:02 -0700

    Added RGB cyclying when there's no alert levels

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Arduino-PC-Monitor
-Title
+
+The CS project is terrible and shouldn't be used. 
+The tutorial (https://create.arduino.cc/projecthub/zakrzu/arduino-pc-monitor-66c07a), while enlightening, is...really bad.
+The python script handles all the PC-side stuff much better. 
--- monitor/monitor.ino
+++ monitor/monitor.ino
@@ -38,6 +38,8 @@
  This example code is in the public domain.
 
  http://www.arduino.cc/en/Tutorial/LiquidCrystal
+
+ potential rgb source: https://forum.arduino.cc/t/color-changing-rgb-led-rainbow-spectrum/8561
  */
 
 // include the library code:
@@ -53,17 +55,24 @@
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
 String inData;
-// define variables
-int redValue;
-int greenValue;
-int blueValue;
 
+// Number of colors used for animating, higher = smoother and slower animation)
+int numColors = 255;
+int counter = 0;
+
+// The combination of numColors and animationDelay determines the
+// animation speed, I recommend a higher number of colors if you want
+// to slow down the animation. Higher number of colors = smoother color changing.
+int animationDelay = 10; // number milliseconds before RGB LED changes to next color
+
+
+// PC vars
 int recentCPULoad = 0;
 int recentCPUTemp = 0;
 int recentRAMLoad = 0;
 int recentRAMUsed = 0;
 
-void setLight() {
+void setLight(unsigned char red, unsigned char green, unsigned char blue) {
 
   if (recentCPULoad > 80 || recentCPUTemp > 100 || recentRAMLoad > 80) {
     digitalWrite(RED, HIGH);
@@ -76,11 +85,87 @@ void setLight() {
     digitalWrite(BLUE, LOW);
   }
   else {
-    digitalWrite(RED, LOW);
-    digitalWrite(GREEN, HIGH);
-    digitalWrite(BLUE, LOW);
+    //digitalWrite(RED, LOW);
+    //digitalWrite(GREEN, HIGH);
+    //digitalWrite(BLUE, LOW);
+    // If we aren't d isplaying a warning light, just continue to cycle RGB
+    setColor(red, green, blue);
   }
   
+}
+
+void setColor(unsigned char red, unsigned char green, unsigned char blue)
+{
+  analogWrite(RED, red);
+  analogWrite(GREEN, green);
+  analogWrite(BLUE, blue);
+}
+
+// Translate all our params to a hard rgb value
+long HSBtoRGB(float _hue, float _sat, float _brightness) {
+  float red = 0.0;
+  float green = 0.0;
+  float blue = 0.0;
+  
+  if (_sat == 0.0) {
+    red = _brightness;
+    green = _brightness;
+    blue = _brightness;
+  } else {
+    if (_hue == 360.0) {
+      _hue = 0;
+    }
+
+    int slice = _hue / 60.0;
+    float hue_frac = (_hue / 60.0) - slice;
+
+    float aa = _brightness * (1.0 - _sat);
+    float bb = _brightness * (1.0 - _sat * hue_frac);
+    float cc = _brightness * (1.0 - _sat * (1.0 - hue_frac));
+    
+    switch(slice) {
+      case 0:
+        red = _brightness;
+        green = cc;
+        blue = aa;
+        break;
+      case 1:
+        red = bb;
+        green = _brightness;
+        blue = aa;
+        break;
+      case 2:
+        red = aa;
+        green = _brightness;
+        blue = cc;
+        break;
+      case 3:
+        red = aa;
+        green = bb;
+        blue = _brightness;
+        break;
+      case 4:
+        red = cc;
+        green = aa;
+        blue = _brightness;
+        break;
+      case 5:
+        red = _brightness;
+        green = aa;
+        blue = bb;
+        break;
+      default:
+        red = 0.0;
+        green = 0.0;
+        blue = 0.0;
+        break;
+    }
+  }
+  long ired = red * 255.0;
+  long igreen = green * 255.0;
+  long iblue = blue * 255.0;
+  
+  return long((ired << 16) | (igreen << 8) | (iblue));
 }
 
 void setup() {
@@ -97,50 +182,77 @@ void setup() {
 }
 
 void loop() {
-    //lcd.clear();
+  // This part takes care of displaying the
+  // color changing in reverse by counting backwards if counter
+  // is above the number of available colors  
+  float colorNumber = counter > numColors ? counter - numColors: counter;
+  
+  // Play with the saturation and brightness values
+  // to see what they do
+  float saturation = 1; // Between 0 and 1 (0 = gray, 1 = full color)
+  float brightness = .05; // Between 0 and 1 (0 = dark, 1 is full brightness)
+  float hue = (colorNumber / float(numColors)) * 360; // Number between 0 and 360
+  long color = HSBtoRGB(hue, saturation, brightness); 
+
+  // Get the red, blue and green parts from generated color
+  int red = color >> 16 & 255;
+  int green = color >> 8 & 255;
+  int blue = color & 255;
+  
   while (Serial.available() > 0)
+  {
+    
+    char received = Serial.read();
+    
+    lcd.setCursor(0,0);
+
+    inData += received; 
+    
+    if (received == 42) // *
     {
-      
-      char received = Serial.read();
-      
+      inData.remove(inData.length() - 1, 1);
       lcd.setCursor(0,0);
+      lcd.print("CPU: " + inData + "% ");
 
-      inData += received; 
-      
-      if (received == 42) // *
-      {
-        inData.remove(inData.length() - 1, 1);
-        lcd.setCursor(0,0);
-        lcd.print("CPU: " + inData + "% ");
+      recentCPULoad = inData.toInt();
 
-        recentCPULoad = inData.toInt();
-
-        inData = ""; 
-      }
-      if (received == 35) // #
-      {
-        inData.remove(inData.length() - 1, 1);
-        lcd.setCursor(10,0);
-        lcd.print(inData + char(223) + "C");
-        recentCPUTemp = inData.toInt();
-        inData = ""; 
-      }
-      
-      if (received == 36) // $
-      {
-        inData.remove(inData.length() - 1, 1);
-        lcd.setCursor(0,1);
-        lcd.print("RAM: " + inData + "% ");
-        recentRAMLoad = inData.toInt();
-        inData = ""; 
-      }
-      if (received == 38) // &
-      {
-        inData.remove(inData.length() - 1, 1);
-        lcd.setCursor(11,1);
-        lcd.print(inData + "GB");
-        inData = ""; 
-      }
+      inData = ""; 
     }
-    setLight();
+    if (received == 35) // #
+    {
+      inData.remove(inData.length() - 1, 1);
+      lcd.setCursor(10,0);
+      lcd.print(inData + char(223) + "C");
+      recentCPUTemp = inData.toInt();
+      inData = ""; 
+    }
+    
+    if (received == 36) // $
+    {
+      inData.remove(inData.length() - 1, 1);
+      lcd.setCursor(0,1);
+      lcd.print("RAM: " + inData + "% ");
+      recentRAMLoad = inData.toInt();
+      inData = ""; 
+    }
+    if (received == 38) // &
+    {
+      inData.remove(inData.length() - 1, 1);
+      lcd.setCursor(11,1);
+      lcd.print(inData + "GB");
+      inData = ""; 
+    }
+  }
+  setLight(red, green, blue);
+
+  // Counter can never be greater then 2 times the number of available colors
+  // the colorNumber = line above takes care of counting backwards (nicely looping animation)
+  // when counter is larger then the number of available colors
+  counter = (counter + 1) % (numColors * 2);
+
+  // If you uncomment this line the color changing starts from the
+  // beginning when it reaches the end (animation only plays forward)
+  // counter = (counter + 1) % (numColors);
+
+  delay(animationDelay);
 }
